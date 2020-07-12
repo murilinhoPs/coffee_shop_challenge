58caaaf037b84f4e823614bb8be4ea1ce224040b
MuriloPs <murilinhops@pop-os.localdomain>
2020-07-12 01:29:06 -0300

    fixed bottom bar error

--- lib/app/pages/home/components/bottom_bar.dart
+++ lib/app/pages/home/components/bottom_bar.dart
@@ -5,33 +5,34 @@ class BottomBar extends StatelessWidget {
   final AppColors appColors;
   final List<String> bottomBarItems;
 
-  const BottomBar({Key key, this.appColors, this.bottomBarItems}) : super(key: key);
+  const BottomBar({Key key, this.appColors, this.bottomBarItems})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 15),
-        height: MediaQuery.of(context).size.height * 0.1,
-        decoration: BoxDecoration(
-          color: appColors.bottomBarBlue,
-          border: Border(
-            top: BorderSide(
-                style: BorderStyle.solid, color: Colors.white54, width: 0.5),
-          ),
+      padding: EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+      height: MediaQuery.of(context).size.height * 0.1,
+      decoration: BoxDecoration(
+        color: appColors.bottomBarBlue,
+        border: Border(
+          top: BorderSide(
+              style: BorderStyle.solid, color: Colors.white54, width: 0.5),
         ),
-        child: ListView.builder(
-          padding: const EdgeInsets.all(10.0),
-          scrollDirection: Axis.horizontal,
-          itemCount: bottomBarItems.length,
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              bottomBarItems[index].toUpperCase(),
-              overflow: TextOverflow.visible,
-              style: TextStyle(color: appColors.white, fontSize: 17),
-            ),
+      ),
+      child: ListView.builder(
+        padding: const EdgeInsets.all(10.0),
+        scrollDirection: Axis.horizontal,
+        itemCount: bottomBarItems.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            bottomBarItems[index].toUpperCase(),
+            overflow: TextOverflow.visible,
+            style: TextStyle(color: appColors.white, fontSize: 17),
           ),
         ),
       ),
+    );
   }
 }
